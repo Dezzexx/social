f81dd1cd82319f9fdc2a24f25a829910ce486935
Dezzex <dezzex@yandex.ru>
2021-10-21 22:56:15 +0300

    Upload

---
+++ src/components/Profile.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+const Content = () => {
+    return (
+        <content className='content'>
+            <div>
+                <img src='https://st.depositphotos.com/1755257/1283/v/600/depositphotos_12837535-stock-illustration-social-network-background-of-the.jpg'></img>
+            </div>
+            <div>
+                <img src='http://cs622426.vk.me/v622426834/409d/baLqspYwi84.jpg'></img>
+            </div>
+            <div>
+                wall my post
+                <div>
+                    new post
+                </div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </content>
+    )
+}
+export default Content;
---
+++ src/components/navigationbar.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+
+const Navbar = () => {
+    return (
+        <navigationbar className='navbar'>
+            <div>
+                <a>Main page</a>
+            </div>
+
+            <div>
+                <a>feed</a>
+            </div>
+
+            <div>
+                <a>Message</a>
+            </div>
+
+            <div>
+                <a>Music</a>
+            </div>
+
+            <div>
+                <a>Settings</a>
+            </div>
+
+        </navigationbar>
+    )
+}
+export default Navbar;
